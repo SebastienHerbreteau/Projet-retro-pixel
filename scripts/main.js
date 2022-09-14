69fdef460d0ebfec3c3b39915c033de82ff157d0
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2022-09-14 11:53:12 +0200

    version finale

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,89 @@
 let start = document.querySelector(".start");
+let launch = document.querySelector(".launch");
 let intro = document.querySelector(".intro");
 let musique = document.querySelector(".musique");
 let demarre = document.querySelector(".demarre");
 let moteur = document.querySelector(".moteur");
-let convecteurPng= document.querySelector(".convecteurPng");
+let convecteurPng = document.querySelector(".convecteurPng");
 let reset = document.querySelector(".reset");
+let voyage = document.querySelector(".voyage");
+let firstFlash = document.querySelector(".first-flash");
+let affiche = document.querySelector(".affiche");
+let fleche = document.querySelector(".fleche");
 
-
-function rouge(){
-    setInterval(() => {
-        var min = 95;
-        var max = 120;
-    brightness = Math.floor(Math.random() * (max - min) + min) + ("%");
+function rouge() {
+  setInterval(() => {
+    var min = 95;
+    var max = 120;
+    brightness = Math.floor(Math.random() * (max - min) + min) + "%";
     document.querySelector(".rouge").style.filter = `brightness(${brightness})`;
-    console.log(brightness)
-}, 20);
+    console.log(brightness);
+  }, 20);
 }
 
-function vert(){
-    setInterval(() => {
-        var min = 130;
-        var max = 150;
-    brightness = Math.floor(Math.random() * (max - min) + min) + ("%");
+function vert() {
+  setInterval(() => {
+    var min = 130;
+    var max = 150;
+    brightness = Math.floor(Math.random() * (max - min) + min) + "%";
     document.querySelector(".vert").style.filter = `brightness(${brightness})`;
-    console.log(brightness)
-},100);
+    console.log(brightness);
+  }, 100);
 }
 
-function jaune(){
-    setInterval(() => {
-        var min = 80;
-        var max = 100;
-    brightness = Math.floor(Math.random() * (max - min) + min) + ("%");
+function jaune() {
+  setInterval(() => {
+    var min = 80;
+    var max = 100;
+    brightness = Math.floor(Math.random() * (max - min) + min) + "%";
     document.querySelector(".jaune").style.filter = `brightness(${brightness})`;
-    console.log(brightness)
-},1);
+    console.log(brightness);
+  }, 1);
 }
 
+start.onclick = function () {
+  moteur.play();
+  start.classList.add("push");
+  setTimeout(() => {
+    convecteurPng.classList.add("desactive");
+    jaune();
+  }, 2600);
 
-start.onclick=function(){
-    moteur.play();
+  setTimeout(() => {
+    vert();
+  }, 2650);
 
-    setTimeout(() => {
-        convecteurPng.classList.add("desactive");
-        jaune();
-    }, 2700);
-    
-    setTimeout(() => {
-        vert();
-    }, 2750);
+  setTimeout(() => {
+    rouge();
+  }, 2700);
+};
 
-    setTimeout(() => {
-        rouge();
-    }, 2800);
-    };
+reset.onclick = function () {
+  location.reload();
+  return false;
+};
 
-reset.onclick=function(){
-    location.reload();
-return false;
-}
+fleche.onclick = function () {
+  location.reload();
+  return false;
+};
 
+launch.onclick = function () {
+  launch.classList.add("push");
+  setTimeout(() => {
+    voyage.play();
+  }, 1);
 
+  setTimeout(() => {
+    firstFlash.classList.add("animFirstFlash", "active");
+  }, 2000);
 
+  setTimeout(() => {
+    intro.classList.add("desactive");
+  }, 4500);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  setTimeout(() => {
+    musique.play();
+    affiche.classList.replace("desactive", "active");
+  }, 4500);
+};
